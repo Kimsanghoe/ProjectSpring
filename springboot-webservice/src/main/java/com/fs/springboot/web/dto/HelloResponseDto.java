b0ec41115b73abca09d0b36aac07bc483c5fb58a
Kim sang hoe <mace302@naver.com>
2022-07-20 12:53:58 +0900

    20220720
    71P Lombok

---
+++ springboot-webservice/src/main/java/com/fs/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,15 @@
+package com.fs.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+
+}
