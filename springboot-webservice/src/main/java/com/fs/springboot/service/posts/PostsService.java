4e0c46502a16cdf081861a14a1e6b082ebfa76ea
Kim sang hoe <mace302@naver.com>
2022-08-01 12:31:43 +0900

    20220801
    jpa Posts등록test

---
+++ springboot-webservice/src/main/java/com/fs/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.fs.springboot.service.posts;
+
+import com.fs.springboot.domain.posts.PostsRepository;
+import com.fs.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+
+}
